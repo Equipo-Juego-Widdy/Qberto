8bca62a8e9f7f180d9d581212a101e51f12941ec
AxelG10 <axelcaceres134@yahoo.com>
2019-09-30 16:09:40 -0300

    Progreso en el escenario y en los movimientos

--- Escenario Q-bert/main.js
+++ Escenario Q-bert/main.js
@@ -13,11 +13,11 @@ function fullDraw(){
 player.update()
 fullDraw()
 player.animIdle()
-/*var b1 = new block(0,0,true,0)
+var b1 = new block(0,0,true,0)
 var b2 = new block(1,0,false,0)
 var b3 = new block(0,-1,true,1)
 b1.draw()
 b2.draw()
 b3.draw()
-*/
+
 //use a timer and a temp variable to create animation time
